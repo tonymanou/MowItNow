797b05f0201181e7ca34f821f57cf95793bc89c9
tonymanou <tonymanou@gmail.com>
2017-04-20 02:50:40 +0200

    Add base project files

---
+++ src/main/java/com/tonymanou/mowitnow/Main.java
@@ -0,0 +1,12 @@
+package com.tonymanou.mowitnow;
+
+public class Main {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args optional arguments (ignored)
+     */
+    public static void main(String[] args) {
+    }
+}
